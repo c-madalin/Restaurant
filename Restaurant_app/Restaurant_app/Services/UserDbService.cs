26303c1a3b9a06810930ddb657fb3935b54d72bc
[cazan] <cazanmadalin12@gmail.com>
2025-05-20 00:30:36 +0300

    login register

---
+++ Restaurant_app/Restaurant_app/Services/UserDbService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Restaurant_app.Services
+{
+    public static class UserDbService
+    {
+        private static readonly string connectionString =
+            File.ReadAllText("appsettings.json") // sau folosind config provider
+            .Split("\"DefaultConnection\": \"")[1].Split("\"")[0];
+
+        public static void InsertUser(string email, string hash, string firstName, string lastName)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand("INSERT INTO users (email, password_hash, first_name, last_name) VALUES (@e, @p, @f, @l)", conn);
+            cmd.Parameters.AddWithValue("e", email);
+            cmd.Parameters.AddWithValue("p", hash);
+            cmd.Parameters.AddWithValue("f", firstName);
+            cmd.Parameters.AddWithValue("l", lastName);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static string GetPasswordHashByEmail(string email)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand("SELECT password_hash FROM users WHERE email = @e", conn);
+            cmd.Parameters.AddWithValue("e", email);
+            using var reader = cmd.ExecuteReader();
+
+            return reader.Read() ? reader.GetString(0) : null;
+        }
+    }
+
+}
---
+++ Restaurant_app/Restaurant_app/ViewModels/UserAuthViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+using Restaurant_app.Commands;
+using Restaurant_app.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+using Restaurant_app.Services;
+
+namespace Restaurant_app.ViewModels
+{
+
+
+    public class UserAuthViewModel : INotifyPropertyChanged
+    {
+        public User CurrentUser { get; set; } = new User();
+
+        public ICommand RegisterCommand { get; }
+        public ICommand LoginCommand { get; }
+
+        public UserAuthViewModel()
+        {
+            RegisterCommand = new RelayCommand(_ => RegisterUser());
+            LoginCommand = new RelayCommand(_ => LoginUser());
+        }
+
+        private void RegisterUser()
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(CurrentUser.Password);
+
+            // Aici folosești serviciul tău de DB pentru a salva utilizatorul
+            UserDbService.InsertUser(CurrentUser.Email, hashedPassword, CurrentUser.FirstName, CurrentUser.LastName);
+        }
+
+        private void LoginUser()
+        {
+            string storedHash = UserDbService.GetPasswordHashByEmail(CurrentUser.Email);
+
+            if (BCrypt.Net.BCrypt.Verify(CurrentUser.Password, storedHash))
+            {
+                // autentificare reușită
+            }
+            else
+            {
+                // parolă greșită
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
